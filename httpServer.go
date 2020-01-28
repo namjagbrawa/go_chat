2825b943d5be0ad9ee2450fc42549c4e4e3e13ed
ge.zhang <ge.zhang@shangeri-la.com>
2020-01-29 02:13:07 +0800

    parse request

--- httpServer.go
+++ httpServer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,7 +11,28 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello world")
 }
 
+func WechatHandlerAuth(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(body))
+	m := make(map[string]string)
+
+	err := json.Unmarshal(body, &m)
+	if err != nil {
+
+		fmt.Println("Umarshal failed:", err)
+		return
+	}
+
+
+	fmt.Println("m:", m)
+
+	for k,v :=range m {
+		fmt.Println(k, ":", v)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/chat", IndexHandler)
+	http.HandleFunc("/chat/wechat", WechatHandlerAuth)
 	http.ListenAndServe("127.0.0.1:8088", nil)
 }
